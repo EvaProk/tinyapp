b6335ffe7417888c493df3d325fb6bea8accf156
Eva Prokhorenko <evaprokhorenko@gmail.com>
2021-08-30 23:58:50 +0000

    Add the rote to the new urls Form and add body-parser

--- express_server.js
+++ express_server.js
@@ -1,21 +1,29 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser"); // middleware to make POST requests readable
 
-// et the view engine to ejs
-app.set('view engine', 'ejs');
+
+
+app.set('view engine', 'ejs');// add the view engine to ejs
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+
+
+app.get("/urls/new", (req, res) => {   // Handler for the new Urls 
+  res.render("urls_new");
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 })
 
-app.get("/urls/:shortURL", (req, res) => {
+app.get("/urls/:shortURL", (req, res) => {     // Handler(route) for short Urls 
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
