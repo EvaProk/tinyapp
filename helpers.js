1e06a1d526d79b53d8e13f14778f03b4399d260d
Eva Prokhorenko <evaprokhorenko@gmail.com>
2021-09-03 04:45:37 +0000

    Add a sepaate fle for helper functions and a folder with auto tests for a function

---
+++ helpers.js
@@ -0,0 +1,25 @@
+
+const generateRandomString = function() {   // Generates a 6 character string of Letters and numbers
+  let text = "";
+
+  let charset = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let i = 0; i < 6; i++)
+    text += charset.charAt(Math.floor(Math.random() * charset.length));
+
+  return text;
+};
+
+const getUserByEmail = (email, database) => {  //Finds a user by a given email in our database
+  for (const key in database) {
+    const user = database[key];
+    if (user.email === email) {
+      return user;
+    }
+  }
+
+  return null;
+};
+
+
+module.exports = { getUserByEmail, generateRandomString };
---
+++ test/helpersTest.js
@@ -0,0 +1,35 @@
+const { assert } = require('chai');
+
+const { getUserByEmail } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUserByEmail', function () {
+  it('should return a user with valid email', function () {
+    const user = getUserByEmail("user@example.com", testUsers).id;
+    const expectedOutput = "userRandomID";
+    assert.equal(user, expectedOutput)
+  });
+  it('should return UNDEFINED if the email is not in our database', function () {
+    const user = getUserByEmail("user@eple.com", testUsers)
+    assert.equal(user, undefined)
+  });
+  it('should return UNDEFINED if the email is empty', function () {
+    const user = getUserByEmail("", testUsers)
+
+    assert.equal(user, undefined)
+  });
+
+
+});
